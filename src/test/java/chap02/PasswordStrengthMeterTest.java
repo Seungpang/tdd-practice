f7646c4ff07045e250cec28a95ba2fca6acf5bd8
Kim Seunglae <obey1342@gmail.com>
2022-01-03 17:57:23 +0900

    feat: 대문자 포함 조건만 충족하는 경우

--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -48,4 +48,9 @@ public class PasswordStrengthMeterTest {
 	void meetsOnlyNumCriteria_Then_Weak() {
 		assertStrength("12345", PasswordStrength.WEAK);
 	}
+
+	@Test
+	void meetsOnlyUpperCriteria_Then_Weak() {
+		assertStrength("ABZEF", PasswordStrength.WEAK);
+	}
 }
