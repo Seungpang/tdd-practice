1b5e82e36d3b8d50a160c25ab035e38398599195
Kim Seunglae <obey1342@gmail.com>
2022-01-03 18:04:23 +0900

    refactor: meter메서드 리팩토링

--- src/main/java/chap02/PasswordStrengthMeter.java
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -6,26 +6,17 @@ public class PasswordStrengthMeter {
 			return PasswordStrength.INVALID;
 		}
 
-		boolean lengthEnough = s.length() >= 8;
-		boolean containsNum = meetsContainingNumberCriteria(s);
-		boolean containsUpp = meetsContainingUppercaseCriteria(s);
+		int metCounts = 0;
+		if (s.length() >= 8)
+			metCounts++;
+		if (meetsContainingNumberCriteria(s))
+			metCounts++;
+		if (meetsContainingUppercaseCriteria(s))
+			metCounts++;
 
-		if (lengthEnough && !containsNum && !containsUpp) {
+		if (metCounts == 1)
 			return PasswordStrength.WEAK;
-		}
-		if (!lengthEnough && containsNum && !containsUpp) {
-			return PasswordStrength.WEAK;
-		}
-		if (!lengthEnough && !containsNum && containsUpp) {
-			return PasswordStrength.WEAK;
-		}
-		if (!lengthEnough) {
-			return PasswordStrength.NORMAL;
-		}
-		if (!containsNum) {
-			return PasswordStrength.NORMAL;
-		}
-		if (!containsUpp)
+		if (metCounts == 2)
 			return PasswordStrength.NORMAL;
 		return PasswordStrength.STRONG;
 	}
