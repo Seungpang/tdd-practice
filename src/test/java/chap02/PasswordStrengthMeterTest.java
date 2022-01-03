5fbade2ee5ea5312ff3394b01fc5b0fe59819b4d
Kim Seunglae <obey1342@gmail.com>
2022-01-03 17:55:48 +0900

    feat: 숫자 포함 조건만 충족하는 경우 추가

--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -43,4 +43,9 @@ public class PasswordStrengthMeterTest {
 	void meetsOnlyLengthCriteria_Then_Weak() {
 		assertStrength("abcdefghi", PasswordStrength.WEAK);
 	}
+
+	@Test
+	void meetsOnlyNumCriteria_Then_Weak() {
+		assertStrength("12345", PasswordStrength.WEAK);
+	}
 }
