a8a6d235170e9bddeeaca1570184c020054c844d
Kim Seunglae <obey1342@gmail.com>
2022-01-04 19:23:31 +0900

    refactor: PayData로 변경

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -11,18 +11,35 @@ public class ExpiryDateCalculatorTest {
 	@Test
 	void 만원_납부하면_한달_뒤가_만료일이_됨() {
 		assertExpiryDate(
-			LocalDate.of(2022, 1, 1), 10_000,
+			PayData.builder()
+				.billingDate(LocalDate.of(2022, 1, 1))
+				.payAmount(10_000)
+				.build(),
 			LocalDate.of(2022, 2, 1)
 		);
 		assertExpiryDate(
-			LocalDate.of(2022, 3, 3), 10_000,
-			LocalDate.of(2022, 4, 3))
-		;
+			PayData.builder()
+				.billingDate(LocalDate.of(2022, 3, 3))
+				.payAmount(10_000)
+				.build(),
+			LocalDate.of(2022, 4, 3)
+		);
 	}
 
-	private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
+	@Test
+	void 납부일과_한달_뒤_일자가_같지_않음() {
+		assertExpiryDate(
+			PayData.builder()
+				.billingDate(LocalDate.of(2022, 1, 31))
+				.payAmount(10_000)
+				.build(),
+			LocalDate.of(2022, 2, 28)
+		);
+	}
+
+	private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
 		ExpiryDateCalculator cal = new ExpiryDateCalculator();
-		LocalDate realExpiryDate = cal.calculdateExpiryDate(billingDate, payAmount);
+		LocalDate realExpiryDate = cal.calculdateExpiryDate(payData);
 		assertEquals(expectedExpiryDate, realExpiryDate);
 	}
 }
